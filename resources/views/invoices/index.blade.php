d3ae93de842efcafc9003db5db15fd1c43e370b6
mo7mead <mo7mead.wael.dev@gmail.com>
2024-03-12 01:32:18 +0200

    edit

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.master')
+@section('title')
+    ZARTTEX / Invoices
+@endsection
 @section('css')
     <!-- Internal Data table css -->
     <link href="{{ URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" />
